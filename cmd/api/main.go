25c853bd755c0d3b7ed3d270a4ec57ac47eb6888
panjiasmoroart <pandjiasmoroart@gmail.com>
2024-12-31 11:37:02 +0700

    Scaffolding our API Server - Environment Variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/panjiasmoroart/gopher-social/internal/env"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg := config{
-		addr: ":9090",
+		addr: env.GetString("ADDR", ":9090"),
 	}
 
 	app := &application{
