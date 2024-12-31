8964bc75ffcd8ff02fc35303457306c637300e02
panjiasmoroart <pandjiasmoroart@gmail.com>
2024-12-31 14:23:10 +0700

    Databases - Persisting data with SQL

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -5,5 +5,5 @@ import "net/http"
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
 
-	app.store.Posts.Create(r.Context())
+	// app.store.Posts.Create(r.Context())
 }
