a1a7303da439d36fbea81842d2357d3dc01f6a0f
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-01-02 22:29:48 +0700

    Posts CRUD - Adding Comments to Posts

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -76,6 +76,14 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	comments, err := app.store.Comments.GetByPostID(ctx, id)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+	// basically, this is relationship post and comments
+	post.Comments = comments
+
 	if err := writeJSON(w, http.StatusOK, post); err != nil {
 		app.internalServerError(w, r, err)
 		return
