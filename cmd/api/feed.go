8c6863e1ddb86d1c7c6f63b777f7db1be9cd99ef
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-02-06 14:30:31 +0700

    Filtering, Sorting and Pagination - Pagination and Sorting

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,13 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/panjiasmoroart/gopher-social/internal/store"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
-	// pagination, filters
+	// pagination, filters, sort
+	// feed?limit=20&offset=5
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
 
 	ctx := r.Context()
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(351))
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(351), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
